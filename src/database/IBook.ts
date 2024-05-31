f9fa312beb8d13264d645047e09a0a6514340cad
ThomasDluhosch <t.dluhosch@gmail.com>
2024-05-31 18:26:36 +0200

    added Books

--- src/database/IBook.ts
+++ src/database/IBook.ts
@@ -20,5 +20,26 @@ export const someBookExamples :IBook[] = [{
     author: "Prince and Assassin",
     title: "Tavia Lark",
     rating: 9,
-  }
+  },{
+    alreadyRead: false,
+    author: "George Orwell",
+    title: "1984",
+    rating: 9,
+  },{
+    alreadyRead: true,
+    author: "Tim Marshall",
+    title: "Prisoners of Geography",
+    rating: 9,
+  },{
+    alreadyRead: false,
+    author: "Randall Munroe",
+    title: "what if? 2",
+    rating: 9,
+  },{
+    alreadyRead: false,
+    author: "Stephen Hawking",
+    title: "Eine kurze Geschichte der Zeit",
+    rating: 9,
+  },
+  
 ]
